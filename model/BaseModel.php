57da485047d2d84d82c7994a40a9a84055e2e32a
nadee85 <51080036+nadee85@users.noreply.github.com>
2019-09-08 13:36:33 +0530

    Update Model

--- model/BaseModel.php
+++ model/BaseModel.php
@@ -15,49 +15,152 @@ class BaseModel {
 
     //put your code here
     protected $con;
+    protected $id;
 
     public function __construct() {
         $this->con = new mysqli("localhost", "root", "adm!nsb123", "topnotch", "3306");
     }
 
-    public function save($element=NULL) {
+    public function save($element = NULL) {
         if (!isset($element)) {
-            $element=  $this;
+            $element = $this;
         }
-        
-        $reflect=new ReflectionClass($element);
-        $properties   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
-        $fields=array();
-        $values=array();
-        
-        foreach ($properties as $k){
-            if ($k->getName()==="con") {
-                continue;
-            }
-            $fields[]=$k->getName();
-            $k->setAccessible(true);
-            $val=$k->getValue($element);
-            if (gettype($val)!=="Integer") {
-                $val="'$val'";
-            }
-            $values[]=$val;
-            $k->setAccessible(FALSE);
-        }
-        
-            
+
+        $fields = $this->getFields();
+        $values = $this->getFieldValues($element);
+
+
         $fieldList = implode(",", $fields);
         $valueList = implode(",", $values);
-        $tableName=  get_class($element);
-        
-//        echo json_encode($fieldList);
-//        echo json_encode($valueList);
-        
+
+        $tableName = get_class($element);
+
         $query = "INSERT INTO $tableName($fieldList)VALUES ($valueList)";
-        $res=$this->con->query($query);
+        $res = $this->con->query($query);
         if (!$res) {
             echo $this->con->error;
         }
         return $res;
+    }
+
+    public function update($updatedElement = NULL) {
+        $tableName = get_class($this);
+
+        if (!isset($updatedElement)) {
+            $updatedElement = $this;
+        }
+
+        $fieldValueMap = $this->getFieldValueMap($updatedElement);
+
+        $generateSqlKeyValuePairs = function ($k, $v) {
+            return "$k=$v";
+        };
+
+        $valueString = implode(',', $this->array_map_asoc($generateSqlKeyValuePairs, $fieldValueMap));
+
+        if (gettype($this->id) !== "Integer") {
+            $id = "'$this->id'";
+        }
+        
+        $query = "UPDATE $tableName SET $valueString WHERE id=$id";
+
+        $res = $this->con->query($query);
+
+        if (!$res) {
+            $err = $this->con->error_list;
+        }
+    }
+
+    public function find($id) {
+        $tableName = get_class($this);
+        
+        if (gettype($this->id) !== "Integer") {
+            $id = "'$this->id'";
+        }
+        
+        $query = "SELECT * FROM $tableName WHERE id=$id";
+        
+        $res = $this->con->query($query);
+
+        if ($res->num_rows < 0) {
+            return NULL;
+        }
+        
+        $record = $res->fetch_assoc();
+
+        $fields = $this->getFields();
+
+        foreach ($fields as $field) {
+            $this->$field = $record[$field];
+        }
+        
+        return $this;
+    }
+
+    private function getFields() {
+        $reflect = new ReflectionClass($this);
+        $properties = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
+        $fields = array();
+
+        foreach ($properties as $k) {
+            if ($k->getName() === "con") {
+                continue;
+            }
+            $fields[] = $k->getName();
+        }
+        return $fields;
+    }
+
+    private function getFieldValues($element) {
+        $reflect = new ReflectionClass($element);
+        $properties = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
+        $values = array();
+
+        foreach ($properties as $k) {
+            if ($k->getName() === "con") {
+                continue;
+            }
+            $k->setAccessible(true);
+
+            $val = $k->getValue($element);
+            if (gettype($val) !== "Integer") {
+                $val = "'$val'";
+            }
+            $values[] = $val;
+            $k->setAccessible(FALSE);
+        }
+        return $values;
+    }
+
+    private function getFieldValueMap($element) {
+        $reflect = new ReflectionClass($element);
+        $properties = $reflect->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE);
+        $values = array();
+
+        foreach ($properties as $k) {
+            if ($k->getName() === "con") {
+                continue;
+            }
+            $k->setAccessible(true);
+
+            $name = $k->getName();
+            $val = $k->getValue($element);
+            if (gettype($val) !== "Integer") {
+                $val = "'$val'";
+            }
+
+            $values[$name] = $val;
+            $k->setAccessible(FALSE);
+        }
+        return $values;
+    }
+
+    private function array_map_asoc($callback, $array) {
+        $r = array();
+        foreach ($array as $key => $value) {
+            $r[$key] = $callback($key, $value);
+        }
+        return $r;
     }
 
 }
--- model/User.php
+++ model/User.php
@@ -13,55 +13,9 @@
  */
 class User extends BaseModel{
     //put your code here
-    private $userId;
-    private $fName;
-    private $lName;
-    private $email;
-    private $password;
-    
-    public function __construct() {
-        parent::__construct();
-    }
-    
-    function getUserId() {
-        return $this->userId;
-    }
-
-    function getFName() {
-        return $this->fName;
-    }
-
-    function getLName() {
-        return $this->lName;
-    }
-
-    function getEmail() {
-        return $this->email;
-    }
-
-    function getPassword() {
-        return $this->password;
-    }
-
-    function setUserId($userId) {
-        $this->userId = $userId;
-    }
-
-    function setFName($fName) {
-        $this->fName = $fName;
-    }
-
-    function setLName($lName) {
-        $this->lName = $lName;
-    }
-
-    function setEmail($email) {
-        $this->email = $email;
-    }
-
-    function setPassword($password) {
-        $this->password = $password;
-    }
-
-
+    public $id;
+    public $fname;
+    public $lname;
+    public $email;
+    public $password;
 }
